6d105b2812cd07ff7608a5de6a1dc4f4f1ef2240
campereza043 <campereza@udistrital.edu.co>
2025-11-29 22:48:18 -0500

    ola

---
+++ Parcial2/Billar Lennard-Jones/build/CMakeFiles/simulacion.dir/src/Caja.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/simulacion.dir/src/Caja.cpp.o: \
+ /home/debian12/Documentos/UD/Compu2/Parcial2/Billar\ Lennard-Jones/src/Caja.cpp \
+ /usr/include/stdc-predef.h \
+ /home/debian12/Documentos/UD/Compu2/Parcial2/Billar\ Lennard-Jones/include/Caja.h
